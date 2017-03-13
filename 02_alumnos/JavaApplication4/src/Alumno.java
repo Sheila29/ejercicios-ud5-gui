0aa587a55a3ba0c07a627d0e4e9fa549d89e50e8
Sheilapsm <murgia85@gmail.com>
2017-03-13 21:58:49 +0100

    terminado

---
+++ 02_alumnos/JavaApplication4/src/Alumno.java
@@ -0,0 +1,89 @@
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 7FPROG10
+ */
+public class Alumno {
+    
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private String email;
+    
+ 
+    
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellidos
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * @param apellidos the apellidos to set
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+    
+    
+}
